bea7e4c560d864d43fda5a23a9a382f926b73910
courtneyhale <courtneyhale44@gmail.com>
2016-03-04 14:22:48 -0500

    added (255,55) to color.  Moved vector from display to var

--- bouncing-ball.js
+++ bouncing-ball.js
@@ -15,17 +15,16 @@
 
 // the arguments to the constructor function should represent the BouncingBall's starting position
 var BouncingBall = function (startingX, startingY) {
-  this.x = startingX;
-  this.y = startingY;
-  this.radius = 15;
-  this.position = createVector(startingX, startingY);
+  this.position=new p5.Vector(startingX, startingY);
+  this.direction=p5.Vector.random2D();
+  this.direction.setMag(4);
 
 };
 
 //var v1 = createVector(width / 2, height / 2);
 
 BouncingBall.prototype = {
-
+  radius: 15,
   // initialize() is typically only called once, after the object is created.
   initialize: function () {
 
@@ -33,25 +32,49 @@ BouncingBall.prototype = {
 
   // update() should update the state of the object, moving it and updating any properties
   update: function () {
-    this.position.x += random();
-    this.position.y += random();
-    //if (this.y - this.radius <= 0) this.y = this.radius + random(0, 2);
-    //this.checkForBounce();
+    this.position.add(this.direction);
+    this.checkForBounce();
     //this.move();
+
   },
 
-  move: function(){
+  checkForBounce: function () {
+    if (this.position.x > width - this.radius / 2) this.bounceX();
+    if (this.position.x < this.radius / 2) this.bounceX();
+    if (this.position.y > height - this.radius / 2) this.bounceY();
+    if (this.position.y < this.radius / 2) this.bounceY();
+
+  },
+
+  //bounce: function () {
+    //this.bounceX();
+    //this.bounceY();// your code goes here; a hint: this will require making your own variable as well as defining this function
+    // another hint: notice that ball is defined using object literal notation
+  //},
+
+  bounceX: function() {
+    this.direction.x *= -1
+  },
+
+  bounceY: function() {
+    this.direction.y *=-1
+  },
+
+
+  //move: function () {
+    //this.postion.x += this.speed.x; // this line is the same as: this.x = this.x + 1;
+    //this.postion.y +=this.speed.y;
     //this.direction = new p5.Vector(random(-2,2),random(-2,2));
     //this.direction.setMag(2);
-    this.direction=new p5.Vector(2,0);
+
     //this.direction.rotate(random(0;Pi*2));
 
-  },
+//  },
 
   // display() should draw the object to the canvas, using p5 code
   display: function () {
     noStroke();
-    fill(255);
+    fill(255,35);
     ellipse(this.position.x,this.position.y,this.radius*2,this.radius*2)
     //ellipse(v1.x, v1.y, 30, 30);
   },
